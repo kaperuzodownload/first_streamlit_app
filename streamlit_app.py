00e856afd0ad1321f7237ca15d03040184ffbaba
kaperuzodownload <137834567+kaperuzodownload@users.noreply.github.com>
2023-06-26 23:35:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-import streamlit
-streamlit.title('My parents New Healthy Diner')
+import streamlit as st
+st.title('My parents New Healthy Diner')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
